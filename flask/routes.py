0be18e22ba86f6adcd4443815657c99953bbdef3
Rui Zheng <zhengrui315@gmail.com>
2019-12-20 23:11:34 -0600

    removed dist/bundle.js

--- flask/routes.py
+++ flask/routes.py
@@ -3,8 +3,6 @@ from flask import Flask, Blueprint, render_template
 app = Flask(__name__, static_folder='../src', template_folder='../templates')
 # static_folder='../src/dist' or static_folder='../' doesn't work, don't know why
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
 @app.route('/')
 def index():
     print("entering flask index page")
