688b925c08ecbb616273127f16985cfe6419ce6d
Rui Zheng <zhengrui315@gmail.com>
2020-01-11 16:57:20 -0600

    Added hover to google map

--- static/src/components/Marker.js
+++ static/src/components/Marker.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 
 
-const Marker = (props) => {
-    const { color, arena_name, id, } = props;
+const K_SIZE = 20;
 
-    return (
-        <div className='marker'
-            style={{ backgroundColor: color, cursor: 'pointer'}}
-            title={arena_name}
-            onClick={() => window.open("https://www.google.com", "_blank")}
-        />
-    );
+const markerStyle = {
+  // initially any map object has left top corner at lat lng coordinates
+  // it's on you to set object origin to 0,0 coordinates
+  position: 'absolute',
+  width: K_SIZE,
+  height: K_SIZE,
+  left: -K_SIZE / 2,
+  top: -K_SIZE / 2,
+
+  border: '',
+  borderRadius: K_SIZE,
+  backgroundColor: 'blue',
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 10,
+  fontWeight: 'bold',
+  padding: 4,
+  cursor: 'pointer'
+};
+
+const markerHoverStyle = {
+    ...markerStyle,
+    fontSize: 16
 }
 
-export default Marker
+class Marker extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    handleClick() {
+        window.open("https://www.google.com", "_blank");
+    }
+
+    render() {
+        const { color, arena_name, id, } = this.props;
+
+        {/* GoogleMap passes a $hover prop to hovered components. To detect hover it an uses internal mechanism */}
+        const style = this.props.$hover ? markerHoverStyle : markerStyle;
+        console.log("hover status: ", this.props.$hover);
+        return (
+            <div className='marker'
+                style={style}
+                title={arena_name}
+                onClick={this.handleClick}
+            >
+                {this.props.$hover ? this.props.arena_name : ''}
+            </div>
+        )
+    }
+}
+
+export default Marker;
